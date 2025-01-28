7e417c4e711355a881465c1b8207fbe97a35ceec
Duduumendez <nicksoniaudax5@gmail.com>
2025-01-28 20:41:07 +0300

    Delete handlers directory

--- handlers/website.py
+++
@@ -1,9 +0,0 @@
-# handlers/website.py
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def website_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.callback_query.edit_message_text(
-        "Enjoy more info about hacking from Mendez website 👉 [Mendez Website](https://mendez-website.vercel.app/)",
-        parse_mode="Markdown"
-    )
